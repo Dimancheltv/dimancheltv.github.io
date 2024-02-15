4c3874b4b216b9999034150346914c8d9291647d
Dimanche <dimancheltv@gmail.com>
2024-02-15 18:37:01 +0200

    Dpd app ver7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4880f9b9b28a5fd7aef01028689e2649",
-"index.html": "3715f1216a83fc504214ca57f1f5214a",
-"/": "3715f1216a83fc504214ca57f1f5214a",
-"main.dart.js": "4a295f56455732f139ce6ddeeec8e31e",
+"index.html": "00d3038643b7017777c4a9ce04e6cfb9",
+"/": "00d3038643b7017777c4a9ce04e6cfb9",
+"main.dart.js": "812c6e4786a9bdc35ff8ff9c6bd47854",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
